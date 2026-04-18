2f65c6bdd8a46b2704057bb8efc4f1a2f5083dfb
Brett Cooper <brett@thewiznerd.com>
2026-04-18 08:44:16 -0600

    Added grass rendering.

--- packages/rendering/src/entities/DirtRenderer.ts
+++ packages/rendering/src/entities/DirtRenderer.ts
@@ -32,7 +32,39 @@ export class DirtRenderer extends EntityRenderer {
       topEdgeColor: Colors.yellow,
       frontEdgeColor: Colors.darkYellow,
     },
-    grass: placeholder(Colors.green, Colors.darkGreen),
+    grass: {
+      top(ctx, w, h) {
+        ctx.fillStyle = Colors.green
+        ctx.fillRect(0, 0, w, h)
+        ctx.fillStyle = Colors.lightGreen
+        ctx.fillRect(5, 3, 1, 1)
+        ctx.fillRect(14, 1, 1, 1)
+        ctx.fillRect(10, 8, 1, 1)
+      },
+      front(ctx, w, h) {
+        // Dirt base
+        ctx.fillStyle = Colors.yellow
+        ctx.fillRect(0, 0, w, h)
+        ctx.fillStyle = Colors.darkYellow
+        ctx.fillRect(4, 8, 1, 1)
+        ctx.fillRect(11, 4, 1, 1)
+        // Grass strip: row 1 = darkYellow, row 0 = darkGreen with green pixels
+        ctx.fillRect(0, 1, w, 1)
+        ctx.fillStyle = Colors.darkGreen
+        ctx.fillRect(0, 0, w, 1)
+        ctx.fillStyle = Colors.green
+        ctx.fillRect(2, 0, 1, 1)
+        ctx.fillRect(7, 0, 1, 1)
+        ctx.fillRect(10, 0, 1, 1)
+        ctx.fillRect(w - 1, 0, 1, 1)
+      },
+      unknownTop(ctx, w, h) {
+        ctx.fillStyle = Colors.darkGreen
+        ctx.fillRect(0, 0, w, h)
+      },
+      topEdgeColor: Colors.darkGreen,
+      frontEdgeColor: Colors.darkYellow,
+    },
   }
 
   readonly terrain = true
